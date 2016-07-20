d564ee4ca3f8bb33b33d11337fc55fdba92241e8
Vu Nguyen <vu@vnguyen.io>
2016-07-20 12:15:08 -0400

    MCL v0.1
    TODO: getClusters()

--- SocialNetworks/src/graph/MCL.java
+++ SocialNetworks/src/graph/MCL.java
@@ -1,5 +1,86 @@
 package graph;
 
-public class MCL {
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import org.ujmp.core.*;
+import org.ujmp.core.calculation.Calculation;
 
+public class MCL {
+	static final int NUM_OF_AUTHORS = 5000;
+	static Map<Integer, String> authors = new HashMap<>();
+	static Map<Integer, Integer> authorIndices = new HashMap<>();
+	static SparseMatrix adjMatrix = SparseMatrix.Factory.zeros(NUM_OF_AUTHORS, NUM_OF_AUTHORS);
+		
+	public static void loadMatrix(SparseMatrix m, String filename) {
+        Scanner sc;
+        try {
+            sc = new Scanner(new File(filename));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        // Iterate over the lines in the file, adding new
+        // vertices as they are found and connecting them with edges.
+        while (sc.hasNextInt()) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            
+            // graph is undirected, hence matrix is symmetrical
+            m.setAsDouble(1, i, j);
+            m.setAsDouble(1, j, i);
+        }
+        
+        sc.close();
+    }
+	
+	/*public static void loadDefinition(Map m, String filename) {
+		return;
+	}*/
+	
+	public SparseMatrix normalize(SparseMatrix m) {
+		Matrix s = m.sum(Calculation.NEW, 0, false); // column-wise sum
+		SparseMatrix r = SparseMatrix.Factory.zeros(NUM_OF_AUTHORS, NUM_OF_AUTHORS);
+		for (int col = 0; col < NUM_OF_AUTHORS; col++) {
+			for (int row = 0; row < NUM_OF_AUTHORS; row++) {
+				r.setAsDouble(m.getAsDouble(row, col)/s.getAsDouble(0, col), row, col);
+			}
+		}
+		
+		return r;
+	}
+	
+	public SparseMatrix inflate(SparseMatrix m, double inflate_factor) {
+		return normalize((SparseMatrix) m.power(Calculation.ORIG, inflate_factor));
+	}
+	
+	public SparseMatrix expand(SparseMatrix m, int expand_factor) {
+		for (int i = 0; i < expand_factor; i++) {
+			m = (SparseMatrix)m.times(m);
+		}
+		return m;
+	}
+	
+	public SparseMatrix selfLoop(SparseMatrix m, double mult_factor) {
+		return (SparseMatrix) m.plus(SparseMatrix.Factory.eye(NUM_OF_AUTHORS).times(mult_factor));
+	}
+	
+	public boolean stop(SparseMatrix m, double epsilon) {
+		SparseMatrix r = (SparseMatrix)m.times(m).minus(m);
+		
+		if (Math.abs(r.getMaxValue() - r.getMinValue()) < epsilon) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void main() {
+		final String FILENAME = "data\\DBLP_APA.txt";
+		
+		loadMatrix(adjMatrix, FILENAME);
+
+		System.out.println(adjMatrix);
+	}
 }
--- SocialNetworks/src/graph/UJMP.java
+++ SocialNetworks/src/graph/UJMP.java
@@ -1,43 +1,37 @@
 package graph;
 
 import org.ujmp.core.*;
-import org.ujmp.core.util.MathUtil;
+import org.ujmp.core.calculation.Calculation;
+//import org.ujmp.core.util.MathUtil;
 
 public class UJMP {
 
+	public static SparseMatrix normalize(SparseMatrix m) {
+		Matrix s = m.sum(Calculation.NEW, 0, false); // column-wise sum
+		SparseMatrix r = SparseMatrix.Factory.zeros(m.getSize(0), m.getSize(1));
+		for (int col = 0; col < m.getSize(1); col++) {
+			for (int row = 0; row < m.getSize(0); row++) {
+				r.setAsDouble(m.getAsDouble(row, col)/s.getAsDouble(0, col), row, col);
+			}
+		}
+		
+		return r;
+	}
+	
 	public static void main(String[] args) {
 		// create a very large sparse matrix
-		SparseMatrix m1 = SparseMatrix.Factory.zeros(1000000, 500000);
-
-		// set some values
-		m1.setAsDouble(MathUtil.nextGaussian(), 0, 0);
-		m1.setAsDouble(MathUtil.nextGaussian(), 1, 1);
-		for (int i = 0; i < 10000; i++) {
-			m1.setAsDouble(MathUtil.nextGaussian(), MathUtil.nextInteger(0, 1000), MathUtil.nextInteger(0, 1000));
-		}
-
-		// show on screen
-		//m1.showGUI();
-
-		// create another matrix
-		SparseMatrix m2 = SparseMatrix.Factory.zeros(3000000, 500000);
-
-		// set some values
-		m2.setAsDouble(MathUtil.nextGaussian(), 0, 0);
-		m2.setAsDouble(MathUtil.nextGaussian(), 1, 1);
-		for (int i = 0; i < 10000; i++) {
-			m2.setAsDouble(MathUtil.nextGaussian(), MathUtil.nextInteger(0, 1000), MathUtil.nextInteger(0, 1000));
-		}
-
-		// show on screen
-		//m2.showGUI();
-
-		// do some operations
-		Matrix m3 = m1.mtimes(m2.transpose());
-
+		SparseMatrix m = SparseMatrix.Factory.zeros(5, 4);
+		m.setAsDouble(0.5, 0, 0);
+		m.setAsDouble(0.25, 1, 0);
+		m.setAsDouble(0.25, 0, 1);
+		m.setAsDouble(0.5, 3, 2);
+		m.setAsDouble(0.1, 3, 3);
+		m.setAsDouble(0.7, 4, 3);
+		System.out.println(m);
+		Matrix s = normalize(m);
+		System.out.println(s);
+		
 		System.out.println("Done");
-		// show on screen
-		//m3.showGUI();
 	}
 
 }
